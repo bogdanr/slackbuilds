6b9bb03aeedb164df615742b40d2b34a36ba75e2
Bogdan Radulescu <bogdan@nimblex.net>
2014-07-08 17:40:02 +0300

    Added: prelink, libelf, wine Upgraded: systemd to 215

--- systemd/doinst.sh
+++ systemd/doinst.sh
@@ -3,6 +3,10 @@ if [[ -f etc/group ]]; then
   if ! grep ^systemd-journal: etc/group 1> /dev/null 2> /dev/null ; then
 	echo "systemd-journal:x:60:" >> etc/group
   fi
+
+  if ! grep ^systemd-timesync: etc/group 1> /dev/null 2> /dev/null ; then
+	echo "systemd-timesync:x:61:" >> etc/group
+  fi
 fi
 
 ( cd lib64 ; ln -sf libudev.so.1.4.0 libudev.so.0 )
