dc63a8603d7d7a8dd436ea987207641412e212e3
Bogdan Radulescu <bogdan@nimblex.net>
2014-08-29 05:56:05 +0300

    Added: mesa which is not dependent of LLVM.
    LLVM became a hard dependency in Gallium3D for mesa especially for
    R300 and RadeonSI. Since LLVM is very large for now we'll do
    withought these drivers.
    
    Upgraded systemd to 216

--- systemd/doinst.sh
+++ systemd/doinst.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-if [[ -f etc/group ]]; then
-  if ! grep ^systemd-journal: etc/group 1> /dev/null 2> /dev/null ; then
-	echo "systemd-journal:x:60:" >> etc/group
-  fi
 
-  if ! grep ^systemd-timesync: etc/group 1> /dev/null 2> /dev/null ; then
-	echo "systemd-timesync:x:61:" >> etc/group
-  fi
-fi
-
-( cd lib64 ; ln -sf libudev.so.1.4.0 libudev.so.0 )
+( cd lib64 ; ln -sf libudev.so.1.6.0 libudev.so.0 )
 
